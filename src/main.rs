d619d4bdabe514e79953b48530c562fd53e9b88c
Brian Jewkes <bdjewkes@gmail.com>
2017-06-29 16:57:45 -0700

    Extremely crude websocket implementation

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,16 @@ extern crate serde_json;
 extern crate serde_derive;
 #[macro_use]
 extern crate rocket_contrib;
+extern crate ws;
 
 use cors::CORS;
 use rocket::State;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use rocket_contrib::{JSON, Value};
 use std::collections::HashMap;
-use std::sync::Mutex;
+use std::sync::{RwLock,Mutex};
 use std::thread;
-use std::time::Duration;
+use ws::{connect, listen, CloseCode, Sender, Handler, Message, Result};
 
 #[derive(Serialize, Deserialize)]
 struct Point(usize, usize);
@@ -30,23 +31,23 @@ impl Point {
     }
 
     fn up(&mut self, distance: usize) {
-        if self.get_y() < HEIGHT - 1 {
-            self.1 += 1;
+        if self.get_y() < HEIGHT - distance {
+            self.1 += distance;
         }
     }
     fn down(&mut self, distance: usize) {
-        if self.get_y() > 0 {
-            self.1 -= 1;
+        if self.get_y() >= distance {
+            self.1 -= distance;
         }
     }
     fn left(&mut self, distance: usize) {
-        if self.get_x() > 0 {
-            self.0 -= 1;
+        if self.get_x() >= distance {
+            self.0 -= distance;
         }
     }
     fn right(&mut self, distance: usize) {
-        if self.get_x() < WIDTH - 1 {
-            self.0 += 1;
+        if self.get_x() < WIDTH - distance {
+            self.0 += distance;
         }
     }
     fn json(&self) -> JSON<Value> {
@@ -54,22 +55,23 @@ impl Point {
     }
 }
 
-type PositionMap = Mutex<HashMap<usize, Point>>;
+type PositionMap = RwLock<HashMap<usize, Point>>;
 
 const HEIGHT: usize = 10;
 const WIDTH: usize = 10;
 
 #[get("/position/reset")]
 fn reset(position_map_state: State<PositionMap>) -> CORS<JSON<Value>> {
-    let mut position_map = position_map_state.lock().unwrap();
+    let mut position_map = position_map_state.write().unwrap();
     position_map.clear();
     CORS::any(JSON(json!("OK")))
 }
 
 #[get("/position")]
 fn position(position_map_state: State<PositionMap>) -> CORS<JSON<Value>> {
-    let position_map = position_map_state.lock().unwrap();
+    let position_map = position_map_state.read().unwrap();
     let results: Vec<&Point> = position_map.iter().map(|(_, point)| point).collect();
+    send_message(serde_json::to_string(&results).unwrap());
     CORS::any(JSON(json!(results)))
 }
 
@@ -77,7 +79,7 @@ fn position(position_map_state: State<PositionMap>) -> CORS<JSON<Value>> {
 fn new_position(entity_count: State<AtomicUsize>,
                 position_map_state: State<PositionMap>)
                 -> CORS<JSON<Value>> {
-    let mut position_map = position_map_state.lock().unwrap();
+    let mut position_map = position_map_state.write().unwrap();
     let position_num = entity_count.fetch_add(1, Ordering::Relaxed);
     let point = Point(0, 0);
     position_map.entry(position_num).or_insert(point);
@@ -86,7 +88,7 @@ fn new_position(entity_count: State<AtomicUsize>,
 
 #[get("/position/<id>")]
 fn get_entities(id: usize, position_map_state: State<PositionMap>) -> CORS<Option<JSON<Value>>> {
-    let position_map = position_map_state.lock().unwrap();
+    let position_map = position_map_state.read().unwrap();
     match position_map.get(&id) {
         Some(position) => CORS::any(Some(position.json())),
         None => CORS::any(None),
@@ -98,9 +100,9 @@ fn position_move(id: usize,
                  direction: &str,
                  position_map_state: State<PositionMap>)
                  -> CORS<Option<JSON<Value>>> {
-    let mut position_map = position_map_state.lock().unwrap();
+    let mut position_map = position_map_state.write().unwrap();
 
-    match position_map.get_mut(&id) {
+    let result = match position_map.get_mut(&id) {
         Some(position) => {
             match direction {
                 "left" => position.left(1),
@@ -112,14 +114,54 @@ fn position_move(id: usize,
             CORS::any(Some(position.json()))
         }
         None => CORS::any(None),
+    };   
+    let state: Vec<&Point> = position_map.iter().map(|(_, point)| point).collect();
+    send_message(serde_json::to_string(&state).unwrap());
+    result
+}
+
+struct Server {
+        out: Sender,
+}
+impl Handler for Server {
+
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+        println!("Server got message '{}'. ", msg);
+        self.out.broadcast(msg)
+    }
+
+    fn on_close(&mut self, code: CloseCode, reason: &str) {
+        println!("WebSocket closing for ({:?}) {}", code, reason);
     }
 }
 
+fn send_message(message: String) {
+    let client = thread::spawn(move || {
+        let m = message.as_str();
+        connect("ws://127.0.0.1:3012", |out| {
+            out.send(m);
+
+            move |msg| {
+                println!("{}", msg);
+                out.close(CloseCode::Normal)
+            }
+        })
+    });
+}
+
 fn main() {
+    // Server thread
+    let server = thread::spawn(move || {
+        listen("127.0.0.1:3012", |out| {
+            Server { out: out }
+        })
+    });
+    
     rocket::ignite()
         .mount("/",
                routes![position, position_move, get_entities, new_position, reset])
         .manage(AtomicUsize::new(0))
-        .manage(Mutex::new(HashMap::<usize, Point>::new()))
+        .manage(RwLock::new(HashMap::<usize, Point>::new()))
+        .manage(Mutex::new(server))
         .launch();
 }
